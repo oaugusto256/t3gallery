d204e4013f99ec8e09d801266e8af5721a3ee01f
Otavio Augusto <tavioalves@gmail.com>
2024-12-04 11:44:49 -0300

    chore: Database setup and some other improvements

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ## TODO
 
 - [x] Make it deploy
-- [ ] Scaffold basic UI with mock data
+- [x] Scaffold basic UI with mock data
+- [x] Tidy up build process
 - [ ] Actually se up a database (w/ Vercel postgres)
 - [ ] Attach database to UI
 - [ ] Add authentication
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   'https://utfs.io/f/2wqCeedJluDyFIgXwvQavZAj8o3mQ6uTVLMYpysOdSJkhcHb',
@@ -11,12 +12,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(image => (
-          <div key={image.id} className="w-48">
+        {posts.map((post, index) => (
+          <div key={post + "-" + index}>
+            {post.name}
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
